9068d7785dff201edeb8f792c24c2fa268a30fe1
Skorpiya <110522421+Skorpiya@users.noreply.github.com>
2022-11-30 16:24:37 +0300

    Контрольная написана

---
+++ Семинары/Калькулятор/calculator.js
@@ -0,0 +1,6 @@
+function calculate(params) {
+  const firstNumber = document.querySelector()
+  if (form.sign.value == '+') {
+    form.result.value == form.var1.value + form.var1.value;
+  }
+}
---
+++ Семинары/Семинар 3/Navigation/Forms/forms.js
@@ -0,0 +1,6 @@
+function check(form) {
+  var my_name = document.querySelector('');
+  var my_date = document.querySelector('');
+  var result_p = document.querySelector('');
+  var kit = document.querySelector('');
+}
